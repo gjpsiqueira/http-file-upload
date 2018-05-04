1be851c85f188d6009c27bfe4d203eb8852e0c46
Gustavo Siqueira <gjpsiqueira@gmail.com>
2018-05-03 21:01:05 -0300

    Initial commit

---
+++ backend/src/main/java/com/br/app/httpfileupload/HttpFileUploadApplication.java
@@ -0,0 +1,12 @@
+package com.br.app.httpfileupload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpFileUploadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HttpFileUploadApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/com/br/app/httpfileupload/HttpFileUploadApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.app.httpfileupload;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpFileUploadApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
