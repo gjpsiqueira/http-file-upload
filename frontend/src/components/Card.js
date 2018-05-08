d176cacf75a4f92e619c006120788a9dd0f90f5a
Gustavo Siqueira <gjpsiqueira@gmail.com>
2018-05-08 19:09:24 -0300

    improve css card bootstrap

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -4,8 +4,8 @@ export default class Card extends React.Component {
 
     render() {
         return (
-            <div className="card fat">
-                <div className="card-body">
+            <div className="card fat border-0">
+                <div className="card-body ">
                     {this.props.children}
                 </div>
             </div>
