f26cab9949f3bdaabd132c1af522b937d1284e91
Gustavo Siqueira <gjpsiqueira@gmail.com>
2018-05-08 16:18:42 -0300

    add dispatch doLogout

--- frontend/src/components/dashboard/NavMenu.js
+++ frontend/src/components/dashboard/NavMenu.js
@@ -1,15 +1,21 @@
 import React from 'react'
+import { doLogout } from '../../actions/authActions'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
-export default class NavMenu extends React.Component {
+class NavMenu extends React.Component {
 
     render() {
         return (
             <div className="nav-scroller bg-white box-shadow">
             <nav className="nav nav-underline">
               <a className="nav-link active" href="#">Lista de arquivos</a>
-              <a className="nav-link" href="#">Sair</a>
+              <a className="nav-link" href="" onClick={() => this.props.doLogout()}>Sair</a>
             </nav>
           </div>            
         )
     }
 }
+
+const mapDispatchToProps = dispatch => bindActionCreators({doLogout},dispatch)
+export default connect(null,mapDispatchToProps)(NavMenu)
